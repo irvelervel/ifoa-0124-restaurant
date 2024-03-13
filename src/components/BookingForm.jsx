68c74b3272d7599b574b5848f6e817803c553b83
Stefano Casasola <ste.casasola@gmail.com>
2024-03-13 11:51:05 +0100

    Controls every input field and adds Al Bano easter egg

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 // import { Form, Button } from 'react-bootstrap' <-- questo metodo anche funziona, ma non è ottimale
 
 // questo form creerà una prenotazione in un database sfruttando delle API
@@ -38,17 +39,62 @@ class BookingForm extends Component {
           <Form.Group className="mb-3">
             <Form.Label>Il tuo nome</Form.Label>
             {/* i Form.Control si traducono in HTML in <input /> */}
-            <Form.Control type="text" placeholder="Mario Rossi" required />
+            <Form.Control
+              type="text"
+              placeholder="Mario Rossi"
+              required
+              onChange={
+                // qui dentro ci va una funzione che viene eseguita ad ogni cambiamento dell'input
+                (e) => {
+                  this.setState({
+                    // dobbiamo FONDERE un nuovo oggetto all'interno del nostro this.state
+                    reservation: {
+                      ...this.state.reservation, // riportando qui dentro TUTTE le proprietà di reservation
+                      name: e.target.value, // il carattere che ho appena scritto
+                    },
+                  })
+                }
+              }
+              value={this.state.reservation.name}
+            />
           </Form.Group>
+
+          {this.state.reservation.name === 'Al Bano' && (
+            <Alert variant="success">FELICITÀ!</Alert>
+          )}
 
           <Form.Group className="mb-3">
             <Form.Label>Numero di telefono</Form.Label>
-            <Form.Control type="tel" placeholder="320xxxxxxx" required />
+            <Form.Control
+              type="tel"
+              placeholder="320xxxxxxx"
+              required
+              onChange={(e) => {
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation, // riportando qui dentro TUTTE le proprietà di reservation
+                    phone: e.target.value,
+                  },
+                })
+              }}
+              value={this.state.reservation.phone}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>In quanti siete?</Form.Label>
-            <Form.Select aria-label="Default select example">
+            <Form.Select
+              aria-label="Default select example"
+              onChange={(e) => {
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation, // riportando qui dentro TUTTE le proprietà di reservation
+                    numberOfPeople: e.target.value,
+                  },
+                })
+              }}
+              value={this.state.reservation.numberOfPeople}
+            >
               <option>1</option>
               <option>2</option>
               <option>3</option>
@@ -62,16 +108,52 @@ class BookingForm extends Component {
 
           <Form.Group className="mb-3">
             <Form.Label>Data / Ora</Form.Label>
-            <Form.Control type="datetime-local" required />
+            <Form.Control
+              type="datetime-local"
+              required
+              onChange={(e) => {
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation, // riportando qui dentro TUTTE le proprietà di reservation
+                    dateTime: e.target.value,
+                  },
+                })
+              }}
+              value={this.state.reservation.dateTime}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
-            <Form.Check type="checkbox" label="Tavolo fumatori?" />
+            <Form.Check
+              type="checkbox"
+              label="Tavolo fumatori?"
+              onChange={(e) => {
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation, // riportando qui dentro TUTTE le proprietà di reservation
+                    smoking: e.target.checked, // checked è true/false, value nelle checkboxes sarebbe "on"/"off"
+                  },
+                })
+              }}
+              checked={this.state.reservation.smoking}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Allergie, intolleranze, richieste speciali?</Form.Label>
-            <Form.Control as="textarea" rows={5} />
+            <Form.Control
+              as="textarea"
+              rows={5}
+              onChange={(e) => {
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation, // riportando qui dentro TUTTE le proprietà di reservation
+                    specialRequests: e.target.value,
+                  },
+                })
+              }}
+              value={this.state.reservation.specialRequests}
+            />
           </Form.Group>
 
           <Button variant="success" type="submit">
